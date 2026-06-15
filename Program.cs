c69ae5f166338fb243b399ce82cb9b862c9b70b8
Jeferson <jefersonfbh2010@gmail.com>
2026-06-15 20:42:28 -0300

    Questão 4

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
-Console.WriteLine("informe a temperatura atual: ");
-int temperatura = int.Parse(Console.ReadLine()!);
-if (temperatura >26)
-{
-    Console.WriteLine("ambiente quente!");
-}
-else  if  (temperatura >=18)
-{
-    Console.WriteLine ("Ambiente agradavel!");
-}
-    else
-    {
-        Console.WriteLine ("Ambiente frio! ");
-    }
-     
-    
+Console.WriteLine("Informe o valor total da compra:");
 
+double valorCompra = double.Parse(Console.ReadLine());
+
+double desconto;
+double valorFinal;
+
+if (valorCompra >= 200)
+{
+    desconto = valorCompra * 0.10;
+}
+else
+{
+    desconto = 0;
+}
+
+valorFinal = valorCompra - desconto;
+
+Console.WriteLine("Valor original: R$ " + valorCompra);
+Console.WriteLine("Desconto aplicado: R$ " + desconto);
+Console.WriteLine("Valor final: R$ " + valorFinal);
