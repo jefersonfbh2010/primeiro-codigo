205e30393cdab13f64d2a2e1cddcd25b0cc74560
Jeferson <jefersonfbh2010@gmail.com>
2026-06-15 22:04:49 -0300

    questão 8

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
-Console.Write("Informe o primeiro número: ");
-        int num1 = int.Parse(Console.ReadLine());
+Console.Write("Informe a primeira pontuação: ");
+        int p1 = int.Parse(Console.ReadLine()!);
 
-        Console.Write("Informe o segundo número: ");
-        int num2 = int.Parse(Console.ReadLine());
+        Console.Write("Informe a segunda pontuação: ");
+        int p2 = int.Parse(Console.ReadLine()!);
 
-        if (num1 > num2)
+        Console.Write("Informe a terceira pontuação: ");
+        int p3 = int.Parse(Console.ReadLine()!);
+
+        // Caso todas sejam iguais
+        if (p1 == p2 && p2 == p3)
         {
-            Console.WriteLine("O primeiro número é maior.");
-        }
-        else if (num2 > num1)
-        {
-            Console.WriteLine("O segundo número é maior.");
+            Console.WriteLine("As três pontuações são iguais.");
         }
         else
         {
-            Console.WriteLine("Os números são iguais.");
+            int maior = p1;
+
+            if (p2 > maior)
+            {
+                maior = p2;
+            }
+
+            if (p3 > maior)
+            {
+                maior = p3;
+            }
+
+            Console.WriteLine($"Maior pontuação: {maior}");
         }
