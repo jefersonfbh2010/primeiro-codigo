0f7592b5a105afb47bc7129e49ecd5550a0223bb
Jeferson <jefersonfbh2010@gmail.com>
2026-06-15 21:01:27 -0300

    questão 5

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-Console.WriteLine("Informe o valor total da compra:");
+Console.WriteLine("Informe a quantidade de tarefas concluídas:");
 
-double valorCompra = double.Parse(Console.ReadLine()!);
+int tarefas = int.Parse(Console.ReadLine()!);
 
-double desconto = 0;
-
-if (valorCompra >= 200)
+if (tarefas >= 50)
 {
-    desconto = valorCompra * 0.10;
+    Console.WriteLine("Meta atingida.");
+    Console.WriteLine("Tarefas concluídas: " + tarefas);
 }
+else
+{
+    int faltaram = 50 - tarefas;
 
-double valorFinal = valorCompra - desconto;
-
-Console.WriteLine("Valor original: R$ " + valorCompra);
-Console.WriteLine("Desconto aplicado: R$ " + desconto);
-Console.WriteLine("Valor final: R$ " + valorFinal);
+    Console.WriteLine("Meta não atingida.");
+    Console.WriteLine("Faltaram " + faltaram + " tarefas.");
+}
