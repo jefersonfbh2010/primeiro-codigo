cb8b094c47ef4abcaeed71a5f68d8ed0d02fe252
Jeferson <jefersonfbh2010@gmail.com>
2026-06-15 22:21:21 -0300

    questão 14

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
-Console.WriteLine("Informe o número da tabuada:");
-        int numero = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Informe o primeiro valor:");
+        double v1 = double.Parse(Console.ReadLine()!);
 
-        for (int i = 1; i <= 10; i++)
-        {
-            Console.WriteLine($"{numero} x {i} = {numero * i}");
-        }
+        Console.WriteLine("Informe o segundo valor:");
+        double v2 = double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Informe o terceiro valor:");
+        double v3 = double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Informe o quarto valor:");
+        double v4 = double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Informe o quinto valor:");
+        double v5 = double.Parse(Console.ReadLine()!);
+
+        double soma = v1 + v2 + v3 + v4 + v5;
+        double media = soma / 5;
+
+        Console.WriteLine($"Soma dos valores: {soma}");
+        Console.WriteLine($"Média dos valores: {media}");
+    
